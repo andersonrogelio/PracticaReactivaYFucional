3c330172aef90e55ff86e6273869c3fd8034fb6f
Anderson <andersonrogelio0@gmail.com>
2022-08-09 12:35:02 -0300

    Utilizo y muestro mi total con un 25% de iva

--- PracticaMentoria.ts
+++ PracticaMentoria.ts
@@ -2,3 +2,14 @@
 // la cantidad sin IVA y el porcentaje de IVA a aplicar, y devolver el total de la factura. Si se invoca 
 // la función sin pasarle el porcentaje de IVA, deberá aplicar un 21%.
 // eddi.garcia@sofka.com.co
+
+function calculoIva(precio,iva){
+    if (iva == 0){
+        return precio * 0.21;
+    }
+    return precio * iva;
+}
+let facturaSinIva = [10,2.5,100];
+// let facturaConIva  = facturaSinIva.map(precio => calculoIva(precio,0.25));
+let facturaConIva  = facturaSinIva.reduce((total,producto) => total += calculoIva(producto,0.25),0);
+console.log("Total de factura con 25% de IVA:"+ facturaConIva);
